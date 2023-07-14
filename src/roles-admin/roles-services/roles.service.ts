41a16555c95a62e41a87c3b0dc5900483e1a453c
Napssters <niko.orozc@gmail.com>
2023-07-13 20:29:24 -0500

    ajustes faltantes

--- src/roles-admin/roles-services/roles.service.ts
+++ src/roles-admin/roles-services/roles.service.ts
@@ -9,59 +9,66 @@ import { uuid } from 'uuid';
 @Injectable()
 export class RolesService {
 
-    constructor(
-        @InjectRepository(Roles)
-        private readonly userRepository: Repository<Roles>,
-      ) { }
+  constructor(
+    @InjectRepository(Roles)
+    private readonly userRepository: Repository<Roles>,
+  ) { }
 
-    async findByName(name: string): Promise<Roles | null> {
-        try {
-          return await this.userRepository.findOne({ where: { name } });
-        } catch (error) {
-          console.log(error.message);
-          throw new Error('Failed to find rol');
-        }
+  async findByName(name: string): Promise<Roles | null> {
+    try {
+      return await this.userRepository.findOne({ where: { name } });
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to find rol');
+    }
+  }
+
+  async create(createRol: CreateRolDTO): Promise<Roles> {
+    try {
+      return await this.userRepository.save({
+        id: uuid,
+        name: createRol.name,
+        isDeleted: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to create role');
+    }
+
+  }
+
+  async update(name: string, updatedData: Partial<updateRolRequestDTO>): Promise<Roles> {
+    try {
+      const rol = await this.userRepository.findOne({ where: { name } });
+      if (!rol) {
+        throw new Error('Entity not found');
       }
-    
-      async create(createRol: CreateRolDTO): Promise<Roles> {
-        try {
-          return await this.userRepository.save({
-            id: uuid,
-            name: createRol.name,
-            isDeleted: false,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          });
-        } catch (error) {
-          console.log(error.message);
-          throw new Error('Failed to create role');
-        }
-        
+
+      updatedData.updatedAt = new Date();
+      const updatedEntity = { ...rol, ...updatedData };
+
+      return await this.userRepository.save(updatedEntity);
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to update rol');
+    }
+  }
+
+  async softDelete(name: string): Promise<string> {
+    try {
+      const rol = await this.userRepository.findOne({ where: { name } });
+
+      if (!rol) {
+        throw new Error('Entity not found');
       }
-    
-      async update(name: string, updatedData: Partial<updateRolRequestDTO>): Promise<Roles> {
-        try {
-          const rol = await this.userRepository.findOne({ where: { name } });
-          if (!rol) {
-            throw new Error('Entity not found');
-          }
-      
-          const updatedEntity = { ...rol, ...updatedData };
-    
-          return await this.userRepository.save(updatedEntity);
-        } catch (error) {
-          console.log(error.message);
-          throw new Error('Failed to update rol');
-        }
-      }
-    
-      async softDelete(name: string): Promise<string> {
-        try {
-          await this.userRepository.update(name, { isDeleted: true });
-          return "Rol deleted successfully.";
-        } catch (error) {
-          console.log(error.message);
-          return "Failed to delete rol.";
-        }
-      }
- }
+
+      await this.userRepository.update(rol.id, { isDeleted: true });
+      return "Rol deleted successfully.";
+    } catch (error) {
+      console.log(error.message);
+      return "Failed to delete rol.";
+    }
+  }
+}
